465315c236635950ee9bfd772f6459c7b3a00765
Mamdouidar <mamdouidar@gmail.com>
2022-01-29 15:10:28 +0200

    Get all or Single Report(s) and Delete a specific Report

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -19,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::apiResource('/users', UserController::class)->only(['update', 'destroy', 'show']);
+Route::apiResource('/reports', ReportController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
